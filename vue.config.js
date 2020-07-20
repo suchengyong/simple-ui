ba90f9db0f42c0dc81e0cf6725df9075cdc24c48
suchengyong <suchengyong@toweringsecurities.com>
2020-07-20 16:00:59 +0800

    更新

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'? '/simple-ui/': '/',
+  publicPath: process.env.NODE_ENV === 'production'? '/page-simple-ui/': '/',
   css: {
     loaderOptions: {
       sass: {  //webpack 配置 scss 根目录
